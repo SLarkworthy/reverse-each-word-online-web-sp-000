813b9a2952430c37520d94deae3d3d32985672c9
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-01-28 01:46:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,10 @@
 def reverse_each_word(string)
   split_string = string.split(" ")
+  gnirts_array = []
   gnirts = ""
   split_string.each do |word|
-    gnirts << word.reverse + " "
+    gnirts_array << word.reverse
   end
+  gnirts = gnirts_array.join(" ")
   return gnirts
 end
