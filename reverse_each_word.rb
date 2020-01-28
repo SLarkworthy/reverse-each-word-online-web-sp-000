cd0d37e97217f5fd2972f6dd6293df56d2284ff1
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-01-28 01:44:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   split_string = string.split(" ")
   gnirts = ""
   split_string.each do |word|
-    gnirts << word.reverse
+    gnirts << word.reverse + " "
   end
   return gnirts
 end
